20fb0b024bd458a8aefb30c1b302f25a751615cc
Steffan Egyepong <egyepongsteffan@gmail.com>
2026-05-15 15:27:32 -0400

    wrote main/testing code

--- src/main/java/cms/Main.java
+++ src/main/java/cms/Main.java
@@ -4,10 +4,30 @@ package cms;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class Main {
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            System.out.println("Usage: java Main <input.csv> <output.txt>");
+            return;
+        }
+
+        String inputPath  = args[0];
+        String outputPath = args[1];
+
+        GridReader reader = new GridReader();
+        Maze maze = reader.readMaze(inputPath);
+        if (maze == null) {
+            return; 
+        }
+
+        boolean solved = maze.solve();
+
+        if (!solved) {
+            System.out.println("No solution exists for this maze.");
+            return; 
+        }
+
+        GridWriter writer = new GridWriter();
+        writer.writeMaze(maze, outputPath);
     }
 }
